7c4cbece24ebbeee56541b159d97cb002e7662de
louischarnay <louis.charnay@orange.fr>
2024-02-13 17:42:43 +0100

    first commit

--- Architecture réseau pour IOT/TP2/build/bbc-microbit-classic-gcc/ym/ble-nrf51822/source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeremy.deplace/Documents/home/Documents/iot/tp1/microbit-samples/build/bbc-microbit-classic-gcc/ym/ble-nrf51822/source
-# Build directory: /home/jeremy.deplace/Documents/home/Documents/iot/tp1/microbit-samples/build/bbc-microbit-classic-gcc/ym/ble-nrf51822/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Architecture réseau pour IOT/TP2/build/bbc-microbit-classic-gcc/ym/microbit-dal/source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeremy.deplace/Documents/home/Documents/iot/tp1/microbit-samples/yotta_modules/microbit-dal/source
-# Build directory: /home/jeremy.deplace/Documents/home/Documents/iot/tp1/microbit-samples/build/bbc-microbit-classic-gcc/ym/microbit-dal/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
