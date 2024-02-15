e23e4938069b5aa85481af91591c20b66e01e874
Leon Dumestre <ldumestre@groupeastek.fr>
2024-02-15 16:42:42 +0100

    fix: map & add marker

--- app/src/main/java/fr/cpe/pokemon_geo/ui/navigation/AppNavigation.kt
+++ app/src/main/java/fr/cpe/pokemon_geo/ui/navigation/AppNavigation.kt
@@ -1,6 +1,7 @@
 package fr.cpe.pokemon_geo.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -9,10 +10,11 @@ import fr.cpe.pokemon_geo.ui.screen.map.OsmdroidMapView
 import fr.cpe.pokemon_geo.ui.screen.pokedex.Pokedex
 
 @Composable
-fun AppNavigation(navController: NavController) {
+fun AppNavigation(navController: NavController, modifier: Modifier) {
     NavHost(
         navController = navController as NavHostController,
-        startDestination = Routes.POKEDEX
+        startDestination = Routes.POKEDEX,
+        modifier = modifier
     ) {
         composable(route = Routes.POKEDEX) {
             Pokedex()
